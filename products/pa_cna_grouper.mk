de75eb2cd5c8b52f9f265fe97c987962df8e54bc
aaronpoweruser <atlrudeboi1@gmail.com>
2012-10-23 19:36:59 -0400

    redo for better props
    Change-Id: I0c1afdc2a0ee679617974b65621d34c0229cc91a

---
+++ products/pa_cna_grouper.mk
@@ -0,0 +1,20 @@
+$(call inherit-product, device/asus/grouper/full_grouper.mk)
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/cna/config/common_full_hybrid_wifionly.mk)
+
+PRODUCT_COPY_FILES +=  \
+    vendor/cna/prebuilt/nexus7/bootanimation.zip:system/media/bootanimation.zip
+
+OVERLAY_TARGET := pa_tvdpi
+
+# Release name
+PRODUCT_RELEASE_NAME := Nexus7
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasi/grouper:4.1.1/JRO03D/402395:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 4.1.1 JRO03D 402395 release-keys"
+
+PRODUCT_NAME := pa_cna_grouper
+PRODUCT_DEVICE := grouper
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Nexus 7
+PRODUCT_MANUFACTURER := Asus
